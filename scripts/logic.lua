ca1b4d97ffa5c0603ac41781ad5954ffb6e33f7b
Fairweather-Furry <kylehen9758@gmail.com>
2025-02-11 03:42:22 -0500

    Added tower unlocks, icons still needed. logic added for certain medals that require monkey classes

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -1,9 +1,33 @@
 function has(item, amount)
-    local count = Tracker:ProviderCountForCode(item)
-    amount = tonumber(amount)
-    if not amount then
-      return count > 0
-    else
-      return count >= amount
-    end
+  local count = Tracker:ProviderCountForCode(item)
+  amount = tonumber(amount)
+  if not amount then
+    return count > 0
+  else
+    return count >= amount
+  end
+end
+
+function primaryonly()
+  if has("DartMonkey-TUnlock") or has("BoomerangMonkey-TUnlock") or has("BombShooter-TUnlock") or has("TackShooter-TUnlock") or has("IceMonkey-TUnlock") or has("GlueGunner-TUnlock") then
+    return 1
+  else
+    return 0
+  end
+end
+
+function militaryonly()
+  if has("SniperMonkey-TUnlock") or has("MonkeySub-TUnlock") or has("MonkeyBuccaneer-TUnlock") or has("MonkeyAce-TUnlock") or has("HeliPilot-TUnlock") or has("DartlingGunner-TUnlock") or has("MortarMonkey-TUnlock") then
+    return 1
+  else
+    return 0
+  end
+end
+
+function magiconly()
+  if has("WizardMonkey-TUnlock") or has("SuperMonkey-TUnlock") or has("NinjaMonkey-TUnlock") or has("Alchemist-TUnlock") or has("Druid-TUnlock") or has("Mermonkey-TUnlock") then
+    return 1
+  else
+    return 0
+  end
 end
