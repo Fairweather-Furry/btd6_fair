c96fbfd29673102cb9a39426dcf8c5b92d23ccbe
Fairweather-Furry <kylehen9758@gmail.com>
2025-03-21 04:17:48 -0400

    Advanced Maps locations, items, ect

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -1,4 +1,4 @@
 ITEM_MAPPING = {
-    [1] = {"medal","consumable"}, --Medals
-    [2] = {"money","consumable"} --Monkey Money
+--    [1] = {"medal","consumable"}, --Medals
+--    [2] = {"money","consumable"} --Monkey Money
 }
